501236b32153af445a1747d168547e09eac6f015
qiuzhifei <qiuzhifei521@gmail.com>
2021-08-26 17:58:00 +0800

    feat: update deploy.sh

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# 1. Bump the version in https://github.com/QiuZhiFei/swift-commands/blob/dev/Commands.podspec#L3.
-# 2. Bump the git tag and push tag to github, create a release in https://github.com/QiuZhiFei/swift-commands/tags.
-# 3. Publish a cocoapods podspec, Run `pod trunk push Commands.podspec --allow-warnings --verbose`.
+set -euxo pipefail
+
+# test
+## 1. Build and run tests, Run `swift test -v`
+## 2. Validates a Pod, Run `pod lib lint --verbose`
+swift test -v
+pod lib lint --verbose
+
+# deploy
+## 1. Bump the version in https://github.com/QiuZhiFei/swift-commands/blob/dev/Commands.podspec#L3.
+## 2. Bump the git tag and push tag to github, create a release in https://github.com/QiuZhiFei/swift-commands/tags.
+## 3. Publish a cocoapods podspec, Run `pod trunk push Commands.podspec --allow-warnings --verbose`.
