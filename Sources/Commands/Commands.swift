c8518c9e2487a437d6ba99ee404a0fa4e14e5183
QiuZhiFei@users.noreply.github.com <QiuZhiFei>
2024-12-04 11:16:16 +0000

    Automated deployment to GitHub Pages: 5b856036361139505820ea0fc39f8da8f71ad1d4

--- Sources/Commands/Commands.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Commands.swift
-//  
-//
-//  Created by zhifei qiu on 2021/8/2.
-//
-
-public enum Commands { }
--- Tests/CommandsTests/Resources/python/main.py
+++
@@ -1,2 +0,0 @@
-import base64;
-print(base64.b64encode(b'qiuzhifei').decode('ascii'));
